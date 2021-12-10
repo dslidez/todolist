a4a744131adb9bad7d7131e695dcf0124326cb00
Daniil <danya.zinovenko@mail.ru>
2021-12-10 12:17:13 +0300

    refactoring

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Todolist() {
     return (
          <div>
-                <h3>What to learn</h3>
+                <h3></h3>
                 <div>
                     <input/>
                     <button>+</button>
