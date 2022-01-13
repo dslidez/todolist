5a0ac42aa8de12198f6136647ccda07d7fbd2d3f
Daniil <danya.zinovenko@mail.ru>
2022-01-13 18:46:47 +0300

    06-todolists-04

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
+import AddItemForm from './AddItemForm';
 import { FilterValuesType } from './App';
 
 export type TaskType = {
@@ -75,50 +76,6 @@ function Todolist(props: PropsType) {
             </div>
     )
 }
-
-type AddItemFormPropsType = {
-    addItem: (title: string) => void
-}
-
-function AddItemForm(props: AddItemFormPropsType) {
-
-    let [title, setTitle] = useState("")
-    let [error, setError] = useState<string | null> (null)
-
-    const addItem = () => {
-        if (title.trim() !== "") {
-            props.addItem(title);
-            setTitle("");
-        } else {
-            setError("Title is required")
-        }
-    }
- 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
-    }
-
-    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        setError(null);
-        if (e.key === 'Enter') {
-            addItem();
-        }
-    }
-
-    return <div>
-                <input 
-                    value={title} 
-                    onChange={onChangeHandler}
-                    onKeyPress={ onKeyPressHandler }
-                    className={error ? "error" : ""}
-                    />
-                    <button onClick={addItem}>+</button>
-
-                    { error && <div className='error-message'>{error}</div>}
-            </div>
-}
-
-
 
 
 
