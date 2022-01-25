23829bd1b7f42dfe90196c924e207dd9c477a691
Daniil <danya.zinovenko@mail.ru>
2022-01-25 16:51:32 +0300

    all reducers with tests

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,4 +1,4 @@
-import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer} from './tasks-reducer';
+import {addTaskAC, changeTaskStatusAC, changeTaskTitlesAC, removeTaskAC, tasksReducer} from './tasks-reducer';
 import {TasksStateType} from '../App';
 
 test('correct task should be deleted from correct array', () => {
@@ -81,5 +81,25 @@ test('correct task should be added to correct array', () => {
     expect(endState["todolistId1"][1].isDone).toBeTruthy();
   });
  
-
+  test('title of specified task should be changed', () => {
+    const startState: TasksStateType = {
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "2", title: "milk", isDone: true },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    };
+ 
+    const action = changeTaskTitlesAC("2", "Milkyway", "todolistId2");
+ 
+    const endState = tasksReducer(startState, action)
+ 
+    expect(endState["todolistId2"][1].title).toBe("Milkyway");
+    expect(endState["todolistId1"][1].title).toBe("JS");
+  });
 
--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -12,15 +12,21 @@ export type addTaskACActionType={
     todolistId: string
     title: string
 }
-export type changeTaskStatusAC={
+export type changeTaskStatusACActionType={
     type: 'CHANGE-TASK-STATUS'
     todolistId: string
-    id: string
+    taskId: string
     isDone: boolean
+}
+export type changeTaskTitleACActionType={
+    type: 'CHANGE-TASK-TITLE'
+    todolistId: string
+    taskId: string
+    title: string
 }
 
 
-type ActionsType = removeTaskACActionType | addTaskACActionType | changeTaskStatusAC
+type ActionsType = removeTaskACActionType | addTaskACActionType | changeTaskStatusAC | changeTaskTitleAC
 
 export const tasksReducer = (state: Array<TasksStateType>, action: ActionsType) => {
     switch (action.type) {
@@ -49,6 +55,15 @@ export const tasksReducer = (state: Array<TasksStateType>, action: ActionsType) 
             }
             return stateCopy;
         }
+        case 'CHANGE-TASK-TITLE': {
+            const stateCopy = {...state}
+            const tasks = stateCopy[action.todolistId];
+            let task = tasks.find(t => t.id === action.taskId);
+            if (task) {
+              task.title = action.title;
+            }
+            return stateCopy;
+        }
         default:
             throw new Error(" I don't understand this type ")
     }
@@ -62,6 +77,10 @@ export const addTaskAC = (  title: string, todolistId: string ): addTaskACAction
     return { type: 'ADD-TASK', title, todolistId }
  }
 
-export const changeTaskStatusAC = ( id: string, isDone: boolean, todolistId: string): addTaskACActionType => {
-    return { type: 'ADD-TASK', id, todolistId, isDone  }
+export const changeTaskStatusAC = ( taskId: string, isDone: boolean, todolistId: string): changeTaskStatusACActionType => {
+    return { type: 'CHANGE-TASK-STATUS', taskId, todolistId, isDone  }
  }
+ 
+export const changeTaskTitlesAC = (taskId: string, title: string, todolistId: string): changeTaskTitleACActionType => {
+    return { type: 'CHANGE-TASK-TITLE', taskId,  title, todolistId }
+}
