98b7718a1ef51575403f3fb72a090113394d6a7f
Daniil <danya.zinovenko@mail.ru>
2022-04-04 13:17:27 +0300

    Updates

---
+++ precache-manifest.dd92a45c659c4f7fd75f137e40a1af56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dff687e6481b1ed989622061d4e57f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3c2a57d217233f7b89d7",
+    "url": "/todolist/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2c0577704e67f5704fe1",
+    "url": "/todolist/static/js/2.46ad9cc7.chunk.js"
+  },
+  {
+    "revision": "99cd3cebcbf57259939eeba86a95779f",
+    "url": "/todolist/static/js/2.46ad9cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2a57d217233f7b89d7",
+    "url": "/todolist/static/js/main.a3118755.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
